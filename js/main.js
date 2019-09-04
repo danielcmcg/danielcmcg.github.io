6c70dee5e43ba2e6c5529b307a30b01ecf2cee33
Daniel C Menezes <d.cavalcante.m@gmail.com>
2019-09-04 13:32:48 -0300

    update main.js
    making the creature follow the floor heigth,
    making floor move in relation to mouse,
    general adjusteements

--- js/main.js
+++ js/main.js
@@ -44,7 +44,18 @@ window.onload = function() {
         radius: 30,
         gravity: 0,
         bounce: -1,
-        update: function(){
+        update: function(vyVar){
+            
+            var idealDistFromGround = (numberOfSegments*armLength)*0.7;
+            var actualDistFromGround = vyVar-this.y;
+            
+            if(idealDistFromGround > actualDistFromGround){
+                this.vy = -0.3;
+            }
+            else{
+                this.vy = +0.3;
+            }
+            
             this.x += this.vx;
             this.y += this.vy;
             this.vy += this.gravity;
@@ -67,6 +78,7 @@ window.onload = function() {
         },
         render: function(context, mouseX, mouseY){
             context.beginPath();
+            
             context.arc(this.x, this.y, this.radius, 0, Math.PI*2);
             context.fillStyle = "#000";
             context.fill();
@@ -82,7 +94,6 @@ window.onload = function() {
             context.arc(x, y, this.radius/3, 0, Math.PI*2);
             context.fillStyle = "#fff";
             context.fill();
-            
         },
         antcpX: 0,
         antcpY: 0,
@@ -93,34 +104,64 @@ window.onload = function() {
             this.antcpY = this.y + Math.sin(angle) * length;
         }
     };
-
-    var holders = [];
-    var ball1 = {
-        x: 100,
-        y: 100,
-        radius: 20,
-        create: function(x, y) {
-            var obj = Object.create(this);
-            obj.init(x, y);
-            return obj;
-        },
-        init: function(x, y) {
-            this.x = x;
-            this.y = y;
-        },
-        render: function(context){
-            context.beginPath();
-            context.arc(this.x, this.y, this.radius, 0, Math.PI*2);
-            context.fillStyle = "#6e8281";
-            context.fill();
-            //context.fillText(this.x+","+this.y, this.x, this.y+30);
-        }
-    };
     
-    var floor = {
+    var holders = [];
+    var holder = {
         x: 100,
         y: 100,
+        constantX: 0,
+        constantY: 0,
         radius: 20,
+        back: false,
+        timer: false,
+        update: function(mouseX, mouseY){
+            
+            var distanceFromMouse = Math.sqrt(Math.pow(this.x-mouseX,2) + Math.pow(this.y-mouseY,2));
+            var t;
+            if(distanceFromMouse < 150){
+                var angle = Math.atan2(mouseY-this.y, mouseX-this.x);
+                var length = 2;
+                var x = -Math.cos(angle) * length;
+                var y = -Math.sin(angle) * length;
+                
+                var distanceFromBegin = Math.sqrt(Math.pow(this.x-this.constantX,2) + Math.pow(this.y-this.constantY,2));
+                if(distanceFromMouse < 100 && distanceFromBegin < 100){
+                    this.x += x;
+                    this.y += y;
+                }
+                
+                this.timer = true;
+                this.back = false;
+                clearTimeout(t);
+            }
+            else{
+                
+                var holder = this;
+                if(this.timer == true){
+                    t = setTimeout(function(){
+                        holder.back = true;
+                        holder.timer = false;
+                    }, 10000);
+                }
+                
+                if(this.back == true){
+                    var returnVel = 0.3;
+                    if(this.x < this.constantX){
+                        this.x += returnVel;
+                    }
+                    if(this.x > this.constantX){
+                        this.x -= returnVel;
+                    }
+                    if(this.y < this.constantY){
+                        this.y += returnVel;
+                    }
+                    if(this.y > this.constantY){
+                        this.y -= returnVel;
+                    }
+                }
+                
+            }
+        },
         create: function(x, y, r) {
             var obj = Object.create(this);
             obj.init(x, y, r);
@@ -129,6 +170,8 @@ window.onload = function() {
         init: function(x, y, r) {
             this.x = x;
             this.y = y;
+            this.constantX = x;
+            this.constantY = y;
             this.radius = r;
         },
         render: function(context){
@@ -139,58 +182,23 @@ window.onload = function() {
         }
     };
     
-    var numberOfHolders = width/100;
-    // half screen
+    var numberOfHolders = width/30;
     for(var i = 0; i < numberOfHolders; i++){
-        var x = Math.floor(Math.random() * width/2);
+        var x = i*(width/numberOfHolders);
         var min = height/1.1;
-        var max = height - (height/6);  
+        var max = height - (height/4);  
         var random = Math.floor(Math.random() * (+max - +min)) + +min;
-        var y = random; //height - Math.floor(Math.random() * height/4);
-        
-        holders.push(ball1.create(x, y))
-    }
-    // half screen
-    for(var i = 0; i < numberOfHolders; i++){
-        var x = width - Math.floor(Math.random() * width/2);
-        var min = height/1.1;
-        var max = height - (height/6);  
-        var random = Math.floor(Math.random() * (+max - +min)) + +min;
-        var y = random; //height - Math.floor(Math.random() * height/4);
-        
-        holders.push(ball1.create(x, y))
-    }
-    
-    var floors = [];
-    var numberOffloors = 100;
-    for(var i = 0; i < numberOfHolders; i++){
-        var x = Math.floor(Math.random() * width/2);
-        var y = height - Math.floor(Math.random() * height/5);
-        var min = 15;
-        var max = 70;  
+        var y = random;
+        var min = 10;
+        var max = 30;  
         var random = Math.floor(Math.random() * (+max - +min)) + +min;
         var r = random;
         
-        floors.push(floor.create(x, y, r));
-    }
-    for(var i = 0; i < numberOfHolders; i++){
-        var x = width - Math.floor(Math.random() * width/2);
-        var y = height - Math.floor(Math.random() * height/5);
-        var min = 15;
-        var max = 70;  
-        var random = Math.floor(Math.random() * (+max - +min)) + +min;
-        var r = random;
-        
-        floors.push(floor.create(x, y, r));
+        holders.push(holder.create(x, y, r))
     }
     
-	document.body.addEventListener("mousemove", function(event) {
+    document.body.addEventListener("mousemove", function(event) {
 		mouse.x = event.clientX;
-        mouse.y = event.clientY;
-	});
-    
-    document.body.addEventListener("mousedown", function(event) {
-		 mouse.x = event.clientX;
         mouse.y = event.clientY;
 	});
 
@@ -242,8 +250,8 @@ window.onload = function() {
                 }
                 if(!takenHolders.includes(this.closestHolderID)){
                     takenHolders.push(this.closestHolderID);
+                    this.status = 'moving'
                 }
-                this.status = 'moving'
             }
             else if(this.status == 'moving'){
                 this.distance = Math.sqrt(Math.pow(this.closestHolder.x-ball.antcpX,2) + Math.pow(this.closestHolder.y-ball.antcpY,2));
@@ -283,35 +291,28 @@ window.onload = function() {
 		
         for(var i = 0; i < holders.length; i++){
             holders[i].render(context);
-        }
-        
-        for(var i = 0; i < floors.length; i++){
-            floors[i].render(context);
+            holders[i].update(mouse.x, mouse.y);
         }
         
         targets[0].findClosestHolder();
-        
         if(targets[0].closestHolder){
             iks.reach(targets[0].x, targets[0].y, ball.x+15, ball.y+15);
             iks.render(context);
         }
         
         targets[1].findClosestHolder();
-        
         if(targets[1].closestHolder){
             iks2.reach(targets[1].x, targets[1].y, ball.x+15, ball.y-5);
             iks2.render(context);
         }
         
         targets[2].findClosestHolder();
-        
         if(targets[2].closestHolder){
             iks3.reach(targets[2].x, targets[2].y, ball.x-15, ball.y-5);
             iks3.render(context);
         }
         
         targets[3].findClosestHolder();
-        
         if(targets[3].closestHolder){
             iks4.reach(targets[3].x, targets[3].y, ball.x-15, ball.y+15);
             iks4.render(context);
@@ -330,7 +331,18 @@ window.onload = function() {
         }
         
         ball.render(context, mouse.x, mouse.y);
-        ball.update();
+        
+        var vy = 0;
+        var meanSumOfY = 0;
+        
+        for(var i = 0; i < holders.length; i++){
+            if(takenHolders.includes(i)){
+                meanSumOfY += holders[i].y;
+            }
+        }
+        meanSumOfY = meanSumOfY/takenHolders.length;
+        var groundDist = meanSumOfY;
+        ball.update(groundDist);
         
 		requestAnimationFrame(update);
 	}
