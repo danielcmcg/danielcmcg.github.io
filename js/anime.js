0eade42982f1e9ab81127ede17cbeaddf6720350
Daniel C Menezes <d.cavalcante.m@gmail.com>
2019-09-05 13:19:59 -0300

    updating index.html
    adding contact info on the page

--- js/anime.js
+++ js/anime.js
@@ -1,42 +1,6 @@
 import anime from '../anime-master/src/index.js';
 
-//var profile = document.querySelectorAll('.profile');
-//var name = document.querySelectorAll('.name');
-var icons = document.querySelectorAll('.icon50');
-
-//var timedAnim = anime.timeline({
-//    duration: 1000
-//});
-//
-//timedAnim.add({
-//    targets: profile,
-//    translateY: [-200, 0],
-//    opacity: [0, 1],
-//    easing: 'easeOutElastic(1, .9)'
-//});
-//
-//timedAnim.add({
-//    targets: name,
-//    translateY: [-50, 0],
-//    opacity: [0, 1],
-//    easing: 'easeOutElastic(1, .8)'
-//});
-//
-//anime({
-//    targets: '.icon0',
-//    translateX: [-200, 0],
-//    opacity: [0, 1],
-//    delay: 2000,
-//    easing: 'easeOutElastic(1, .8)'
-//});
-//
-//anime({
-//    targets: '.icon1',
-//    translateX: [200, 0],
-//    opacity: [0, 1],
-//    delay: 2000,
-//    easing: 'easeOutElastic(1, .8)'
-//});
+var icons = document.querySelectorAll('.icon');
 
 function animateButton(target, scale){
     anime.remove(target);
@@ -58,5 +22,8 @@ icons[0].addEventListener('mouseleave', function(){ animateButton(icons[0], 1);}
 
 icons[1].addEventListener('mouseenter', function(){ animateButton(icons[1], hoverEnd);}, false);
 icons[1].addEventListener('mouseleave', function(){ animateButton(icons[1], 1);}, false);
+
+icons[2].addEventListener('mouseenter', function(){ animateButton(icons[2], hoverEnd);}, false);
+icons[2].addEventListener('mouseleave', function(){ animateButton(icons[2], 1);}, false);
 
 
