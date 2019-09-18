3d08898d8e656f648d2d6c31188fe6ed1141bd58
Daniel C Menezes <d.cavalcante.m@gmail.com>
2019-09-18 15:49:32 -0300

    small updates

--- js/anime.js
+++ js/anime.js
@@ -17,6 +17,7 @@ function animateButton(target, scale){
 }
 
 var hoverEnd = 1.3;
+
 icons[0].addEventListener('mouseenter', function(){ animateButton(icons[0], hoverEnd);}, false);
 icons[0].addEventListener('mouseleave', function(){ animateButton(icons[0], 1);}, false);
 
@@ -25,5 +26,8 @@ icons[1].addEventListener('mouseleave', function(){ animateButton(icons[1], 1);}
 
 icons[2].addEventListener('mouseenter', function(){ animateButton(icons[2], hoverEnd);}, false);
 icons[2].addEventListener('mouseleave', function(){ animateButton(icons[2], 1);}, false);
+
+icons[3].addEventListener('mouseenter', function(){ animateButton(icons[3], hoverEnd);}, false);
+icons[3].addEventListener('mouseleave', function(){ animateButton(icons[3], 1);}, false);
 
 
