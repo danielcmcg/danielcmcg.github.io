52b6c89af4676dac58d3994c004531b26c6bd224
Daniel C Menezes <d.cavalcante.m@gmail.com>
2021-02-06 10:25:31 -0300

    update brand image with animation

--- js/anime.js
+++ js/anime.js
@@ -3,17 +3,17 @@ import anime from '../anime-master/src/index.js';
 var icons = document.querySelectorAll('.icon');
 
 function animateButton(target, scale){
-    anime.remove(target);
-    anime({
-        targets: target,
-        keyframes: [
-            {scale: scale}
+  anime.remove(target);
+  anime({
+    targets: target,
+    keyframes: [
+      {scale: scale}
 
-        ],
-        duration: 500,
-        easing: 'easeOutElastic(1, .5)',
-        loop: false
-    });
+    ],
+    duration: 500,
+    easing: 'easeOutElastic(1, .5)',
+    loop: false
+  });
 }
 
 var hoverEnd = 1.3;
@@ -35,3 +35,43 @@ icons[4].addEventListener('mouseleave', function(){ animateButton(icons[4], 1);}
 
 //icons[5].addEventListener('mouseenter', function(){ animateButton(icons[5], hoverEnd);}, false);
 //icons[5].addEventListener('mouseleave', function(){ animateButton(icons[5], 1);}, false);
+
+var brandParent = document.querySelectorAll('.brandParent');
+var brandImages = document.querySelectorAll('.brandAnim');
+
+//animateBrand(brandImages, 0, true);
+animateBrand(brandImages[0], 360, true);
+animateBrand(brandImages[1], -180, true);
+
+//brandParent[0].addEventListener('mouseenter', function()
+//{
+//  animateBrand(brandImages[0], 360, true);
+//  animateBrand(brandImages[1], -180, true);
+//}, false);
+//brandParent[0].addEventListener('mouseleave', function()
+//{
+//  animateBrand(brandImages[0], 0, false);
+//  animateBrand(brandImages[1], 1, false);
+//}, false);
+
+function animateBrand(target, value, loop){
+  anime.remove(target);
+  anime({
+    targets: target,
+    loop: loop,
+    direction: 'alternate',
+    translateX: {
+      value: -128,
+      duration: 0
+    },
+    translateY: {
+      value: -30,
+      duration: 0
+    },
+    rotate: {
+      value: value,
+      duration: 20000,
+      easing: 'easeInOutQuad'
+    }
+  });
+}
